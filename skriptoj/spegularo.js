4bb86471eb51f280152e650cc60a8ca7ca81db4c
Martin Bodin <martin.bodin@ens-lyon.fr>
2014-04-17 20:55:10 +0200

    Starting working on the interface.

--- skriptoj/spegularo.js
+++ skriptoj/spegularo.js
@@ -16,58 +16,111 @@
 // along with Spegularo.  If not, see <http://www.gnu.org/licenses/>.
 
 // This file is meant to be the HTML interface with the rest of the JS files.
+// Note that this is the one that loads everything else, and thus can’t use commons’s functions from other files.
 
-
-var sizeScreen = { x: 70, y: 30 }
-
-// Those objects, which will be initialised later on this file, will be used
-// by other files of this project.
-
-// This object contains the following methods:
-//	— setLevel, which takes a table of size sizeScreen of tainted characters
-//	 ({ t: character (one character-string), c: color (string) }) and prints it.
-var updateLevel
-
-// This object contains the following methods:
-//	— setObjects, takes an arraw of tainted characters and prints it.
-var updateInventary
-
-// This object contains the following methods:
-//	— setCode, which takes an array of code line ({ l: code line (string
-//	 without line-break), e: this line is editable (boolean) }) and set this
-//	 as being the current editable code.
-//	— onCommit, which takes a function of type taking the edited array of
-//	 code line (array of string without line-break) called every time the
-//	 player commits a code.
-var editCode
-
-function initLevel (div){
-	// TODO
-	
-	return {
-		setLevel: function (table){}
-	}
-}
-
-function initInventary (div){
-	// TODO
-	
-	return {
-		setObjects: function (objects){}
-	}
-}
-
-function initEdit (div){
-	// TODO
-	
-	return {
-		setCode: function (lines){},
-		onCommit: function (continuation){}
-	}
-}
-
+// This object is the JavaScript one created by this file, and is the only one to be exported.
+var Spegularo
 
 (function (){
+
+	var sizeScreen = { x: 70, y: 30 }
+
+	// Those objects, which will be initialised later on this file, will be used
+	// by other files of this project.
+
+	// This object contains the following methods:
+	//	— setLevel, which takes a table of size sizeScreen of tainted characters
+	//	 ({ t: character (one character-string), c: color (string) }) and prints it.
+	//	— init, taking a language object and initialising the interface.
+	//	— quit, that removes the level from the HTML interface.  Any further
+	//	 call to any method of this object will then be invalid.
+	var updateLevel
+
+	// This object contains the following methods:
+	//	— setObjects, takes an arraw of tainted characters and prints it.
+	//	— init, taking a language object and initialising the interface.
+	//	— quit, that removes the level from the HTML interface.  Any further
+	//	 call to any method of this object will then be invalid.
+	var updateInventary
+
+	// This object contains the following methods:
+	//	— setCode, which takes an array of code line ({ l: code line (string
+	//	 without line-break), e: this line is editable (boolean) }) and set this
+	//	 as being the current editable code.
+	//	— onCommit, which takes a function of type taking the edited array of
+	//	 code line (array of string without line-break) called every time the
+	//	 player commits a code.
+	//	— init, taking a language object and initialising the interface.
+	//	— quit, that removes the level from the HTML interface.  Any further
+	//	 call to any method of this object will then be invalid.
+	var editCode
+
+
+	// Removes every child of a node.
+	function clearNode (node){
+		var childs = node.childNodes
+
+		for (var i = childs.length - 1; i >= 0; i--)
+			node.removeChild (childs[i])
+	}
+
+
+	function initLevel (div){
+		// TODO
+
+		return {
+			setLevel: function (table){},
+			init: function (){},
+			quit: function (){}
+		}
+	}
+
+	function initInventary (main){
+		var block = document.createElement ("p")
+		var array = document.createElement ("span")
+
+		main.appendChild (block)
+
+		return {
+			setObjects: function (objects){
+					clearNode (array)
+
+					for (var i = 0; i < objects.length; i++){
+						if (i !== 0)
+							array.appendChild (document.createTextNode ("\t"))
+
+						var obj = objects[i]
+						var node = document.createTextNode (obj.t)
+						node.setAttribute ("style", "color: " + obj.c + ";")
+
+						array.appendChild (node)
+					}
+				},
+			init: function (langObj){
+					clearNode (node)
+					clearNode (array)
+
+					langObj.appendText ("inventary", block)
+					block.appendChild (array)
+				},
+			quit: function (){
+					main.removeChild (block)
+				}
+		}
+	}
+
+	function initEdit (div){
+		// TODO
+
+		return {
+			setCode: function (lines){},
+			onCommit: function (continuation){},
+			init: function (){},
+			quit: function (){}
+		}
+	}
+
+
 	var mainId = document.getElementById ("spegulo")
 
 	if (!mainId)
@@ -94,6 +147,13 @@ function initEdit (div){
 
 			mainId.appendChild (script)
 		}
+	}
+
+	{ // Enpacking everything into the object “Spegularo”.
+		Spegularo.sizeScreen = sizeScreen
+		Spegularo.updateLevel = updateLevel
+		Spegularo.updateInventary = updateInventary
+		Spegularo.editCode = editCode
 	}
 }())
 
